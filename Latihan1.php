ee5b341a73fe70fcf6189e883c42af515d6e34c4
LuthfiSyarifHidayatullah <74536413+LuthfiSyarifHidayatullah@users.noreply.github.com>
2020-11-19 11:19:55 +0700

    Add files via upload

---
+++ Latihan1.php
@@ -0,0 +1,12 @@
+<?php 
+ $mahasiswa = [
+     "nama" => "Luthfi syarif",
+     "nrp" => "183040160",
+     "email" => "ufisyarif@unpas.ac.id"
+ ];
+
+ $data = json_encode($mahasiswa);
+ echo $data; 
+ 
+?>
+
---
+++ script.js
@@ -0,0 +1,7 @@
+let mahasiswa = {
+    nama : "Luthfi syarif" ,
+    nrp : "180304160",
+    email : "ufisyarif@gmail.com"
+}
+
+console.log(mahasiswa);
